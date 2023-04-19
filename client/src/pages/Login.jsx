b588c2795b7df26c4d6d498490b948a1e9c7a680
ComradeHonk <bozydar427050@gmail.com>
2023-04-19 12:31:27 +0200

    Added basic styles to login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,6 +1,11 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
+import Button from "react-bootstrap/Button";
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 function Login(){
     const [loginController, setLoginController] = useState('');
@@ -41,15 +46,42 @@ function Login(){
     };
 
     return(
-        <div>
-            <form onSubmit={(event) => handleLogin(event)}>
-                Login: <input value={loginController} onChange={event => setLoginController(event.target.value)} /><br/>
-                Password: <input value={passwordController} onChange={event => setPasswordController(event.target.value)} /><br/>
-                <input type="submit" value="sign in"/>
-                <p>don't have an account- <a href="http://localhost:5173/register">sign up</a> </p>
-                <p>{responseMessage}</p>
-            </form>
-        </div>
+        // <div>
+        //     <form onSubmit={(event) => handleLogin(event)}>
+        //         Login: <input value={loginController} onChange={event => setLoginController(event.target.value)} /><br/>
+        //         Password: <input value={passwordController} onChange={event => setPasswordController(event.target.value)} /><br/>
+        //         <input type="submit" value="sign in"/>
+        //         <p>don't have an account- <a href="http://localhost:5173/register">sign up</a> </p>
+        //         <p>{responseMessage}</p>
+        //     </form>
+        // </div>
+
+        <Container fluid>
+            <Row>
+                <Col className=''>
+                    <h1>Tasks and Challenges</h1>
+                </Col>
+            </Row>
+            <Row>
+                <Col className="m-5 p-3 border border-5 border-light rounded">
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formLogin" onSubmit={(event) => handleLogin(event)}>
+                            <Form.Label>Username</Form.Label>
+                            <Form.Control value={loginController} onChange={event => setLoginController(event.target.value)} type="text" placeholder="Enter username" />
+                        </Form.Group>
+                        <Form.Group className="mb-3" controlId="formPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control value={passwordController} onChange={event => setPasswordController(event.target.value)} type="password" placeholder="Password" />
+                        </Form.Group>
+                        <Button className="w-100" variant="success" type="submit">
+                            Sign in
+                        </Button>
+                        <Form.Text>Don't have an account? <a href="http://localhost:5173/register">Sign up</a></Form.Text>
+                        <Form.Text>{responseMessage}</Form.Text>
+                    </Form>
+                </Col>
+            </Row>
+        </Container>
     );
 }
 export default Login
