e6be4ec8d5a90a7ac2c7d523d433f1dba8a21ead
Ignacy Trocki <110670067+ignactr@users.noreply.github.com>
2023-05-15 21:31:54 +0200

    challenges can now expiry

--- server/routes/showChallenges.js
+++ server/routes/showChallenges.js
@@ -3,9 +3,19 @@ const router = express.Router();
 const challenges = require('../models/Challenges');
 const logCheck = require('../middlewares/logCheck');
 
+async function handleExpiry () {
+  const tasks = await challenges.find();
+  tasks.forEach(async (challenge) => {
+    if(challenge.endDate < Date.now()){
+      await challenges.findByIdAndUpdate(challenge._id, {challengeState: 4});
+    }
+  });
+}
+
 router.post('/',logCheck, async (req, res) => {
     try {
       const userId = req.userId
+      handleExpiry();
       const tasks = await challenges.find();
       res.status(207).send({tasks,userId});
     } catch (error) {
